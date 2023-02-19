7a76aba5fdf80cf0c9466bfb20b5ce0f24d18557
Nhlanhla Bright Zondi <bright1zondi@gmail.com>
2023-02-19 04:25:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints programming is like building a multilingual puzzle, followed by a newline
+ * main - prints programming is like building a multilingual puzzle
+ * followed by a newline
  * Return: 0
  */
 int main(void)
