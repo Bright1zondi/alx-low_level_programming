6ad9da3064d9058fa4dfe4fcc96c9baf983f018a
Nhlanhla Bright Zondi <bright1zondi@gmail.com>
2023-03-22 09:09:23 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -45,7 +45,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = 0;
+	dest[i] = '\0';
 
 	return (dest);
 }
@@ -63,8 +63,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-	len1 = strlen(name);
-	len2 = strlen(owner);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
